71b2c26bf7942c74a60c3bd0c67e75980287898a
yuqingqiu <yuqingqiu93@gmail.com>
2019-07-26 15:26:58 -0700

    Added Login pages

--- shoppinglist/shoppinglist/urls.py
+++ shoppinglist/shoppinglist/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('list/', include('list.urls')),
+    path('', include('list.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
